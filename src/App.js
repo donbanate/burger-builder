22e40baa57206f4b5e1af48e095680e23ec87ca6
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-22 02:59:25 +0800

    Enabling and using css module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className={styles.hello}>Hello</h1>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
